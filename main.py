0a31345d13b7cbc47ae78a7fcd38bff7189c6fcd
assskiller <674478778@qq.com>
2021-01-27 23:46:09 +0800

    tune parameters modification

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import torchvision
 import torchvision.transforms as transforms
 import time
 from MultiLayerLSTM import MultiLayerLSTM
-
+import matplotlib.pyplot as plt
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Hyper-parameters
 sequence_length = 28
 input_size = 28
-hidden_size = 128
-num_layers = 1
+#hidden_size = 128
+#num_layers = 1
 num_classes = 10
 batch_size = 100
-num_epochs = 2
-learning_rate = 0.01
+num_epochs = 5
+#learning_rate = 0.01
 
 # MNIST dataset
 train_dataset = torchvision.datasets.MNIST(root='./data/',
@@ -61,50 +61,86 @@ class RNN(nn.Module):
         return out
 
 
-model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
 
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-start = time.time()
-# Train the model
-total_step = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
 
-        images = images.reshape(-1, sequence_length, input_size).to(device) # 这里的-1应该是表示待定，这里的意思就是把tensor转换为 a * 28 * 28
 
-        # break
-        labels = labels.to(device)
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
 
-        if (i + 1) % 100 == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
 
-end = time.time()
-print(end - start)
-# Test the model
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.reshape(-1, sequence_length, input_size).to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+# 开始tune各种hyper params
+hidden_size_list= [20,50,100,128,256]
+num_layers_list = [1,2,3]
+learning_rate_list = [0.001,0.01,0.1,0.5]
 
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+best_model = None
+acc = 0.1
+best_hidden_size = 0
+best_num_layers = 0
+best_learning_rate = 0
+
+for num_layers in num_layers_list:
+    for hidden_size in hidden_size_list:
+        for learning_rate in learning_rate_list:
+            print("***********************************")
+            print("hidden_size:{}, num_layers:{}, learning_rate:{} ".format(hidden_size,num_layers,learning_rate))
+            # plot settings
+            iters = []
+            losses = []
+            model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
+            # Loss and optimizer
+            criterion = nn.CrossEntropyLoss()
+            optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+            start = time.time()
+            # Train the model
+            total_step = len(train_loader)
+            for epoch in range(num_epochs):
+                for i, (images, labels) in enumerate(train_loader):
+                    images = images.reshape(-1, sequence_length, input_size).to(device) # 这里的-1应该是表示待定，这里的意思就是把tensor转换为 a * 28 * 28
+
+                    # break
+                    labels = labels.to(device)
+                    # Forward pass
+                    outputs = model(images)
+                    loss = criterion(outputs, labels)
+                    # Backward and optimize
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                    if (i + 1) % 100 == 0:
+                        print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                              .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+                        iters.append( (epoch)*600+i+1)
+                        losses.append(loss.item())
+
+            end = time.time()
+            print("时间:",end - start)
+            plt.plot(iters,losses)
+            plt.savefig("hs{}_nl{}_rate{}.jpg".format(hidden_size,num_layers,learning_rate*1000))
+            plt.clf()
+            #plt.show()
+            # Test the model
+            model.eval()
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                for images, labels in test_loader:
+                    images = images.reshape(-1, sequence_length, input_size).to(device)
+                    labels = labels.to(device)
+                    outputs = model(images)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+
+                if( (100 * correct / total) > acc):
+                    acc = (100 * correct / total)
+                    best_model = model
+                    best_hidden_size = hidden_size
+                    best_learning_rate = learning_rate
+                    best_num_layers = num_layers
+                print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+            print("***********************************")
 
 # Save the model checkpoint
-torch.save(model.state_dict(), 'model.ckpt')
+torch.save(best_model.state_dict(), 'model.ckpt')
+print("best acc is {},   hidden_size:{}, num_layers:{}, learning_rate:{} ")
